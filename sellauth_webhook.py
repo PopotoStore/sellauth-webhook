bf7157b1e216623932009da8d983c9679e25f424
PopotoStore <popotostore28@gmail.com>
2025-07-27 18:21:02 +0200

    Update sellauth_webhook.py

--- sellauth_webhook.py
+++ sellauth_webhook.py
@@ -2,6 +2,11 @@ from flask import Flask, request, jsonify
 import requests
 import os
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+
 # --- CONFIGURAZIONE ---
 API_KEY = os.environ.get("API_KEY")  # ✅ Variabile ambiente sicura
 BASE_URL = "https://api.sellauth.com/v1"
